dfd39a75e9457c0fc19d66515e18a0412336ca6e
EzraJRogers <Rogersezra7@gmail.com>
2024-01-15 21:34:03 -0700

    Updated script.js to execute and meet challenge acceptance criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,37 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
+const upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+const lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+const numeric = ("1234567890");
+const special = ("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  console.log(document.getElementById("lowercaseYN").checked)
 
+   let includeLowercase = document.getElementById("lowercaseYN").checked;
+   let includeUppercase = document.getElementById("lowercaseYN").checked;
+   let includeNumbers = document.getElementById("lowercaseYN").checked;
+   let includeSpecial = document.getElementById("lowercaseYN").checked;
+   let passwordLength = document.getElementById("quantity").value;
+   let allowedChars = "";
+   let password = "";
+
+   allowedChars += includeLowercase ? lowerCase : "";
+   allowedChars += includeUppercase ? upperCase : "";
+   allowedChars += includeNumbers ? numeric : "";
+   allowedChars += includeSpecial ? special : "";
+
+  if(allowedChars.length === 0) {
+    window.alert("At least 1 password criteria must be selected");
+    return
+  }
+
+  for(let i=0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars[randomIndex];
+  }
+  
   passwordText.value = password;
-
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
